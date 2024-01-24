84afbc54e36a7b5e7219167c3ded37ae4322a9c0
Антон <anton.prv@yandex.com>
2024-01-24 14:28:08 +0400

    Разбираюсь в SQLAlchemy и Postgres

--- api_requests/request.py
+++ api_requests/request.py
@@ -8,25 +8,23 @@ from ..settings import config
 def main():
     get_city_coord('Калининград')
 
+
 def get_city_coord(city):
-    payload = {'geocode' : city, 'apikey' : config.GEO_KEY, 'format' : 'json'}
+    payload = {'geocode': city, 'apikey': config.GEO_KEY, 'format': 'json'}
     r = requests.get('https://geocode-maps.yandex.ru/1.x', params=payload)
     geo = json.loads(r.text)
     return (geo['response']['GeoObjectCollection']['featureMember'][0]
-                 ['GeoObject']['Point']['pos'])
+    ['GeoObject']['Point']['pos'])
 
 
 def get_weather(city):
     coordinates = get_city_coord(city).split()
-    payload = {'lat' : coordinates[1], 'lon' : coordinates[0],
-               'lang' : 'ru_RU'}
+    payload = {'lat': coordinates[1], 'lon': coordinates[0],
+               'lang': 'ru_RU'}
     r = requests.get('https://api_weather.yandex.ru/v2/forecast',
                      params=payload, headers=config.weather_key)
     weather_data = json.loads(r.text)
     return weather_data
-
-
-
 
 
 if __name__ == '__main__':
--- settings/config.py
+++ settings/config.py
@@ -1,3 +1,4 @@
-GEO_KEY: str = ''
+GEO_KEY: str = '6e0a4853-da0e-4814-94f3-47145d2ab6f3'
 
-WEATHER_KEY: str = {'X-Yandex-API-Key' : GEO_KEY}
+WEATHER_KEY: dict[str, str] = {'X-Yandex-API-Key':
+                                   '703ddb2b-5c8c-4702-a747-d3c4d68c579f'}
